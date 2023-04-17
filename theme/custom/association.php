6d6609bc4c370a5af66bdd84bff9e3df5f1fd3c1
SMB Edition Squad <smbedition@prestashop.com>
2023-04-17 11:38:35 +0200

    Init project

---
+++ theme/custom/association.php
@@ -0,0 +1,10 @@
+<div id="association">
+    <prestashop-accounts></prestashop-accounts>
+</div>
+
+<script>
+    /*********************
+     * PrestaShop Account *
+     * *******************/
+    window.psaccountsVue.init();
+</script>
---
+++ theme/custom/hooks/additional-headers.php
@@ -0,0 +1,19 @@
+<!--Custom fonts-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link
+    rel="stylesheet"
+    type="text/css"
+    href="theme/custom/css/header.css"
+/>
+
+<!-- PUIK not compatible with CSS only for now -->
+<!-- <link
+    rel="stylesheet"
+    href="https://unpkg.com/@prestashopcorp/puik/dist/index.css"
+/> -->
+
+<script src="https://unpkg.com/prestashop_accounts_vue_components@4.1.0"></script>
